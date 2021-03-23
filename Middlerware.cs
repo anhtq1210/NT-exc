43b7cc94256eb142fdee692871f21d565e72c27d
anhtq1210 <81178108+anhtq1210@users.noreply.github.com>
2021-03-24 00:00:05 +0700

    Add files via upload

---
+++ Middlerware.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace WebAppExp
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class Middleware
+    {
+        private readonly RequestDelegate _next;
+
+        public Middleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpaContext httpContext)
+        {
+            foreach(var queryParam in httpContext.Request.Query)
+            {
+                console.Writeline(httpContext.Request.Headers);
+            }
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<Middleware>();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,17 +1,26 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-namespace DigitalClock
+namespace WebAppExp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            ClockPublisher clockPublisher = new ClockPublisher();
-            ClockSubcriber clockSubscriber = new ClockSubcriber();
-            clockSubscriber.Subscribe(clockPublisher);
-
-            clockPublisher.Run();
+            CreateHostBuilder(args).Build().Run();
         }
-            
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
